3c2c2e01e53c12f7d181d19cc6399b6598f07f89
nathan_bettiol <nathan_bettiol@10.1.48.222>
2013-07-31 14:49:25 -0300

    Adicionando  a Classe

---
+++ Revisao/src/br/senai/sc/negocio/Pessoa.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.negocio;
+
+/**
+ * Essa classe representa pessoas do software
+ * @version  1.0 31/07/13
+ * @author nathan_bettiol
+ */
+public class Pessoa {
+    
+   private int id;
+   private String endereco;
+   private String telefone;
+   private String dataCadastro;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+   
+   
+    
+    
+}
