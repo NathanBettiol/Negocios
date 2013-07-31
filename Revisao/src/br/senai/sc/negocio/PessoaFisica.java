729c293b41fdb39a844ce36336f0e265b64cf04f
nathan_bettiol <nathan_bettiol@10.1.48.222>
2013-07-31 15:03:24 -0300

    Pessoa Juridica

---
+++ Revisao/src/br/senai/sc/negocio/PessoaFisica.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.negocio;
+
+/**
+ * Classe que representa todas as Pessoas fisicas
+ *
+ * @version 1.0 31/07/13
+ * @author nathan_bettiol
+ */
+public class PessoaFisica {
+
+    private String cpf;
+    private String nome;
+    private String dataNascimento;
+    private String rg;
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    /**
+     * @return the rg
+     */
+    public String getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+}
