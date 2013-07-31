7fc6556f059a80198718473f92351dd891fe85c3
nathan_bettiol <nathan_bettiol@10.1.48.222>
2013-07-31 15:41:15 -0300

    Pessoa Juridica e Funcionario

--- Revisao/src/br/senai/sc/negocio/Funcionario.java
+++ Revisao/src/br/senai/sc/negocio/Funcionario.java
@@ -14,6 +14,44 @@ public class Funcionario extends  PessoaFisica{
     private String dataAdmissao;
     private String ctps;
     private String cargo;
+
+    
+    public Funcionario(){
+        
+    }
+    
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(String dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
     
     
 }
